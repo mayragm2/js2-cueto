dcd4ee05c42c4147fe158eada75dec7c4d01795f
mayragm2 <mayracueto@gm2dev.com>
2026-08-03 10:10:50 -0300

    feat(prototype-test): add new car and display car info with a console.log

--- prototype-test.js
+++ prototype-test.js
@@ -9,6 +9,8 @@ class Vehicle {
 }
 
 const myFerrari = new Vehicle ("Ferrari", 280);
+const myBora = new Vehicle ("WV Bora", 200);
 console.log (myFerrari);
+console.log(`The best car is the ${myBora.make}, it has a speed of ${myBora.speed} km/h`);
 myFerrari.accelerate(10);
 console.log(myFerrari);
