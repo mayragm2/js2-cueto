603f49a77a3d3f4553e2b3c9379cbeaed3bbc9eb
mayragm2 <mayracueto@gm2dev.com>
2026-08-03 10:23:23 -0300

    feat(prototype-test): add brake method to object Vehicle

--- prototype-test.js
+++ prototype-test.js
@@ -6,11 +6,20 @@ class Vehicle {
     accelerate(amount){
         this.speed += amount;
     }
+    brake(amount){
+        this.speed -= amount;
+    }
 }
 
 const myFerrari = new Vehicle ("Ferrari", 280);
 const myBora = new Vehicle ("WV Bora", 200);
-console.log (myFerrari);
 console.log(`The best car is the ${myBora.make}, it has a speed of ${myBora.speed} km/h`);
-myFerrari.accelerate(10);
-console.log(myFerrari);
+
+console.log (myFerrari);    //Print myFerrari status
+myFerrari.accelerate(10);   //Accelerate myFerrari
+console.log(myFerrari);     //Print myFerrari status
+myFerrari.brake(10);        //Brake myFerrari
+console.log (myFerrari);    //Print myFerrari status
+
+console.log(myFerrari.accelerate === myBora.accelerate);
+
